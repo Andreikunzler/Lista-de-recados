09da98e64585143ed196381d51afcbc2622d337d
João Caetano de Mello <joaomellus0@gmail.com>
2023-12-12 16:23:37 -0300

    implementação da api de usuários

--- js/login.js
+++ js/login.js
@@ -5,57 +5,80 @@ if (form.querySelector('#register')) {
     form.addEventListener('submit', login)
 }
 
-function getForm() {
-    const form = document.querySelector('#form-login');
+form.querySelectorAll('.form-control').forEach(el => {
+    el.addEventListener('input', validationFormStyle);
+});
+
+function validationFormStyle(e) {
+    const input = e.target;
+
+    if (!input.value) {
+        input.parentNode.classList.remove("success");
+        input.parentNode.classList.add("error");
+    } else {
+        input.parentNode.classList.add("success");
+        input.parentNode.classList.remove("error");
+    }
+}
+
+async function registerUser(e) {
+    e.preventDefault();
 
     const data = {
         user: form.user.value,
-        password: form.password.value
+        password: form.password.value,
+        email: form.email.value,
+        image: form.image.value
+    }
+
+    for (const key in data) {
+        if (!data[key]) {
+            alert('Preencha todos os campos do formulário!');
+            return null;
+        }
+    }
+
+    if (await userExists(data.email)) {
+        alert('Este email já está sendo utilizado!');
+        return;
     }
     
-    if (!data.user || !data.password) {
-        alert('Preencha todos os campos!');
-        return null;
+    const created = await createUser(data.user, data.password, data.email, data.image);
+    
+    if (created) {
+        alert('Usuário criado com sucesso!');
+    } else {
+        alert('Erro ao tentar cadastrar!')
     }
 
-    return data;
+    form.reset()
+    form.querySelectorAll('.form-control').forEach(div => div.classList.remove('success'));
 }
 
-function registerUser(e) {
+async function login(e) {
     e.preventDefault();
 
-    const data = getForm();
-
-    if (!data) {
-        return;
+    // Validar dados do formulário
+    const data = {
+        email: form.email.value,
+        password: form.password.value
     }
 
-    if (userExists(data.user)) {
-        alert('Este nome de usuário já está sendo utilizado!');
-        return;
+    for (const key in data) {
+        if (!data[key]) {
+            alert('Preencha todos os campos do formulário!');
+            return null;
+        }
     }
 
-    createUser(data.user, data.password);
-    alert('Usuário criado com sucesso!')
-
-    redirect('login');
-}
-
-function login(e) {
-    e.preventDefault();
-
-    const data = getForm();
-
-    if (!data) {
-        return;
-    }
-
-    if (!userExists(data.user)) {
+    // Validar se o usuário existe
+    if (!( await userExists(data.email) )) {
         alert('Usuário ou senha incorreto!');
         return;
     }
-
-    const user = loginUser(data.user, data.password);
+    
+    // Efetuar login
+    const user = loginUser(data.email, data.password);
 
     if (!user) {
         alert('Usuário ou senha incorreto!');
@@ -68,39 +91,39 @@ function login(e) {
     redirect('notes');
 }
 
-
-function getUsers(user, password) {
-    const data = JSON.parse(localStorage.getItem('users'));
-
-    if (!data) {
-        return [];
-    }
-
-    return data;
+async function getUsers() {
+    return (await api.get('/users')).data;
 }
 
-function userExists(username) {
-    const users = getUsers();
+async function userExists(email) {
+    const users = await getUsers();
 
-    return users.find(user => user.username == username) ? true : false;
+    console.log(users)
+    console.log(users.find(user => user.login === email))
+
+    return users.find(user => user.login == email) ? true : false;
 }
 
-function createUser(username, password) {
-    const user = { username, password };
+async function loginUser(login, password) {
+    const userList = await getUsers();
 
-    const userList = getUsers();
-
-    userList.push(user);
-
-    const json = JSON.stringify(userList);
-
-    localStorage.setItem('users', json);
-}
-
-function loginUser(username, password) {
-    const userList = getUsers();
-
-    const user = userList.find(user => user.username === username && user.password === password);
+    const user = userList.find(user => user.login === login && user.password === password);
 
     return user;
+}
+
+async function createUser(name, password, login, avatar) {
+    const user = { name, password, login, avatar };
+
+    try {
+        const response = await api.post('/users', user);
+    
+        if (response.status === 201) {
+            return true;
+        }
+
+    } catch (error) {
+        console.error('Erro ao cadastrar recado', error);
+        return null;
+    }
 }
